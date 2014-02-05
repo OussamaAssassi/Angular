ce998cf3b6e09ecd1849c128a8b8cd7735dafc35
OussamaAssassi <oussama.assassi@gmail.com>
2014-02-05 16:55:02 +0100

    add clickable phone image

--- angular-phonecat/app/js/controllers.js
+++ angular-phonecat/app/js/controllers.js
@@ -8,6 +8,7 @@ phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
  function ($scope,$http) {
   $http.get('phones/phones.json').success(function(data){
   	$scope.phones = data;
+
   });
 
   $scope.orderProp ='age'
@@ -18,5 +19,10 @@ phonecatControllers.controller('PhoneDetailCtrl',['$scope','$routeParams', '$htt
  function($scope,$routeParams,$http) {
   $http.get('phones/'+$routeParams.phoneId+'.json').success(function(data){
   	$scope.phone = data;
+  	$scope.mainImageUrl = data.images[0];
   });
+
+  $scope.setImage = function(imageUrl) {
+  	$scope.mainImageUrl = imageUrl;
+  }
 }]);
